995bc93acf8f41bbb17bd3bd36f8cd02fdaf7fa3
Atikur Rahman <atikur.ice18@gmail.com>
2023-06-02 19:36:45 +0600

    Error in db.js fixed

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,8 @@ const logger = require('../utils/logger');
 require('dotenv').config();
 
 module.exports = function(){
+    console.log(process.env.DB_URL);
     mongoose.connect(process.env.DB_URL)
         .then(() => console.info(`Connected to DB.. ${process.env.DB_URL}`))
-        .catch(err => logger.info('Error'));
+        .catch(err => logger.info('Error'+err));
 }
